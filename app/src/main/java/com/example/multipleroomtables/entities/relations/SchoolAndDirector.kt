39c8bbafc97e32552bd44818e41c6ac5f924521a
Sapehiasahil1 <officialsahilsapehia03@gmail.com>
2023-10-24 19:40:00 +0530

    Added one to one comments

--- app/src/main/java/com/example/multipleroomtables/entities/relations/SchoolAndDirector.kt
+++ app/src/main/java/com/example/multipleroomtables/entities/relations/SchoolAndDirector.kt
@@ -6,10 +6,11 @@ import com.example.multipleroomtables.entities.Director
 import com.example.multipleroomtables.entities.School
 
 data class SchoolAndDirector(
+    // The Embedded annotation assures that the school entities fields are inserted in the SchoolAndDirector class.
     @Embedded val school: School,
     @Relation(
-        parentColumn = "schoolName",
-        entityColumn = "schoolName"
+        parentColumn = "schoolName", // It refers to the parentColumn (in this case its School Entity)
+        entityColumn = "schoolName" // This column is not the primary key but it is compared to the column that is the primary key
     )
     val director: Director
 )
