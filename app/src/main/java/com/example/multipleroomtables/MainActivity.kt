8f343995d0207f5fde2d2a413f0017777a9a111e
Sapehiasahil1 <officialsahilsapehia03@gmail.com>
2023-10-29 14:01:34 +0530

    Done with Multiple Database

--- app/src/main/java/com/example/multipleroomtables/MainActivity.kt
+++ app/src/main/java/com/example/multipleroomtables/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.multipleroomtables
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,37 +11,71 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import com.example.multipleroomtables.entities.Director
+import com.example.multipleroomtables.entities.School
+import com.example.multipleroomtables.entities.Student
+import com.example.multipleroomtables.entities.Subject
+import com.example.multipleroomtables.entities.relations.StudentSubjectCrossRef
 import com.example.multipleroomtables.ui.theme.MultipleRoomTablesTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("CoroutineCreationDuringComposition")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MultipleRoomTablesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                val dao = SchoolDatabase.getInstance(this).schoolDao
+
+                val directors = listOf(
+                    Director("Mike Litoris", "Jake Wharton School"),
+                    Director("Jack Goff", "Kotlin School"),
+                    Director("Chris P. Chicken", "JetBrains School")
+                )
+                val schools = listOf(
+                    School("Jake Wharton School"),
+                    School("Kotlin School"),
+                    School("JetBrains School")
+                )
+                val subjects = listOf(
+                    Subject("Dating for programmers"),
+                    Subject("Avoiding depression"),
+                    Subject("Bug Fix Meditation"),
+                    Subject("Logcat for Newbies"),
+                    Subject("How to use Google")
+                )
+                val students = listOf(
+                    Student("Beff Jezos", 2, "Kotlin School"),
+                    Student("Mark Suckerberg", 5, "Jake Wharton School"),
+                    Student("Gill Bates", 8, "Kotlin School"),
+                    Student("Donny Jepp", 1, "Kotlin School"),
+                    Student("Hom Tanks", 2, "JetBrains School")
+                )
+                val studentSubjectRelations = listOf(
+                    StudentSubjectCrossRef("Beff Jezos", "Dating for programmers"),
+                    StudentSubjectCrossRef("Beff Jezos", "Avoiding depression"),
+                    StudentSubjectCrossRef("Beff Jezos", "Bug Fix Meditation"),
+                    StudentSubjectCrossRef("Beff Jezos", "Logcat for Newbies"),
+                    StudentSubjectCrossRef("Mark Suckerberg", "Dating for programmers"),
+                    StudentSubjectCrossRef("Gill Bates", "How to use Google"),
+                    StudentSubjectCrossRef("Donny Jepp", "Logcat for Newbies"),
+                    StudentSubjectCrossRef("Hom Tanks", "Avoiding depression"),
+                    StudentSubjectCrossRef("Hom Tanks", "Dating for programmers")
+                )
+                lifecycleScope.launch {
+                    directors.forEach { dao.insertDirector(it) }
+                    schools.forEach { dao.insertSchool(it) }
+                    subjects.forEach { dao.insertSubject(it) }
+                    students.forEach { dao.insertStudent(it) }
+                    studentSubjectRelations.forEach { dao.insertStudentSubjectCrossRef(it) }
+
+                    val schoolWithDirector = dao.getSchoolAndDirectorWithSchoolName("Kotlin School")
+
+                    val schoolWithStudents = dao.getSchoolWithStudents("Kotlin School")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MultipleRoomTablesTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/com/example/multipleroomtables/SchoolDao.kt
+++ app/src/main/java/com/example/multipleroomtables/SchoolDao.kt
@@ -8,8 +8,12 @@ import androidx.room.Transaction
 import com.example.multipleroomtables.entities.Director
 import com.example.multipleroomtables.entities.School
 import com.example.multipleroomtables.entities.Student
+import com.example.multipleroomtables.entities.Subject
 import com.example.multipleroomtables.entities.relations.SchoolAndDirector
 import com.example.multipleroomtables.entities.relations.SchoolWithStudents
+import com.example.multipleroomtables.entities.relations.StudentSubjectCrossRef
+import com.example.multipleroomtables.entities.relations.StudentWithSubjects
+import com.example.multipleroomtables.entities.relations.SubjectWithStudents
 
 @Dao
 interface SchoolDao {
@@ -29,7 +33,22 @@ interface SchoolDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertStudent(student: Student)
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSubject(subject: Subject)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertStudentSubjectCrossRef(crossRef: StudentSubjectCrossRef)
+
     @Transaction
     @Query("SELECT * FROM school WHERE schoolName = :schoolName")
     suspend fun getSchoolWithStudents(schoolName: String): List<SchoolWithStudents>
+
+    @Transaction
+    @Query("SELECT * FROM subject WHERE subjectName = :subjectName")
+    suspend fun getStudentsOfSubject(subjectName: String): List<SubjectWithStudents>
+
+    @Transaction
+    @Query("SELECT * FROM student WHERE studentName = :studentName")
+    suspend fun getSubjectsOfStudent(studentName: String): List<StudentWithSubjects>
+
 }
